b600c08f4f6acba4b9ba4b3d8517ea1d72a6f488
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-11-09 01:53:11 -0200

    Jspdf

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -256,6 +256,10 @@ export class WhatsAppController {
 
       if (this.el.inputDocument.files.length) {
 
+        this.el.panelDocumentPreview.css({
+          'height': '1%'
+        });
+
         let file = this.el.inputDocument.files[0];
 
         this._documentPreviewController = new DocumentPreviewController(file);
@@ -267,9 +271,15 @@ export class WhatsAppController {
           this.el.imagePanelDocumentPreview.show();
           this.el.filePanelDocumentPreview.hide();
 
+          this.el.panelDocumentPreview.css({
+            'height': 'calc(100% - 120px)'
+          });
+
         }).catch(err => {
 
-          console.log(file.type);
+          this.el.panelDocumentPreview.css({
+            'height': 'calc(100% - 120px)'
+          });
 
           switch (file.type) {
 
