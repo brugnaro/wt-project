69ac4a3c050777ab12d6a193a76215ce300d638c
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-11-13 08:57:13 -0200

    Connect to firebase

---
+++ src/util/Firebase.js
@@ -0,0 +1,51 @@
+const firebase = require('firebase');
+require('firebase/firestore');
+
+export class Firebase {
+
+  constructor() {
+
+    this._config = {
+      apiKey: "AIzaSyBGFw4g7v0rROGg1inR8BB_mBhpgqtkRz0",
+      authDomain: "wtproject-663eb.firebaseapp.com",
+      databaseURL: "https://wtproject-663eb.firebaseio.com",
+      projectId: "wtproject-663eb",
+      storageBucket: "wtproject-663eb.appspot.com",
+      messagingSenderId: "85393010399"
+    };
+
+    this.init();
+
+  }
+
+  init() {
+
+    if (!this._initialized) {
+
+      firebase.initializeApp(this._config);
+
+      firebase.firestore().settings({
+        timestampsInSnapshots: true
+      });
+
+      this._initialized = true;
+
+    }
+
+  }
+
+
+  static db() {
+
+    return firebase.firestore();
+
+  }
+
+  static hd() {
+
+    return firebase.storage();
+
+  }
+
+}
+
