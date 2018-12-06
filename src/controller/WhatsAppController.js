d7d5fe0cd271bce7ccff70263dbbcfb551004d35
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-12-05 22:52:27 -0200

    Send message

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -5,6 +5,7 @@ import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from './../util/Firebase';
 import { User } from '../model/User';
 import { Chat } from '../model/Chat';
+import { Message } from '../model/Message';
 
 export class WhatsAppController {
 
@@ -146,21 +147,7 @@ export class WhatsAppController {
 
         div.on('click', e => {
 
-          console.log('chatId',contact.chatId);
-
-          this.el.activeName.innerHTML = contact.name;
-          this.el.activeStatus.innerHTML = contact.status;
-
-          if (contact.photo) {
-            let img = this.el.activePhoto;
-            img.src = contact.photo;
-            img.show();
-          }
-
-          this.el.home.hide();
-          this.el.main.css({
-            display: 'flex'
-          });
+          this.setActiveChat(contact);
 
         });
 
@@ -171,6 +158,26 @@ export class WhatsAppController {
     });
 
     this._user.getContacts();
+  }
+
+  setActiveChat(contact) {
+
+    this._contactActive = contact;
+
+    this.el.activeName.innerHTML = contact.name;
+    this.el.activeStatus.innerHTML = contact.status;
+
+    if (contact.photo) {
+      let img = this.el.activePhoto;
+      img.src = contact.photo;
+      img.show();
+    }
+
+    this.el.home.hide();
+    this.el.main.css({
+      display: 'flex'
+    });
+
   }
 
   loadElements() {
@@ -584,7 +591,17 @@ export class WhatsAppController {
     });
 
     this.el.btnSend.on('click', e => {
-      console.log(this.el.inputText.innerHTML);
+
+      Message.send(
+        this._contactActive.chatId,
+        this._user.email,
+        'text',
+        this.el.inputText.innerHTML
+      );
+
+      this.el.inputText.innerHTML = '';
+      this.el.panelEmojis.removeClass('open');
+
     });
 
     this.el.btnEmojis.on('click', e => {
