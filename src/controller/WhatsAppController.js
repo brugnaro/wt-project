aa811b288b95ef17d952b3b0bf72d27662a967fd
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-12-11 20:58:52 -0200

    Filter contacts

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -158,6 +158,7 @@ export class WhatsAppController {
     });
 
     this._user.getContacts();
+
   }
 
   setActiveChat(contact) {
@@ -310,6 +311,18 @@ export class WhatsAppController {
   }
 
   initEvents() {
+
+    this.el.inputSearchContacts.on('keyup', e => {
+
+      if (this.el.inputSearchContacts.value.length > 0) {
+        this.el.inputSearchContactsPlaceholder.hide();
+      } else {
+        this.el.inputSearchContactsPlaceholder.show();
+      }
+
+      this._user.getContacts(this.el.inputSearchContacts.value);
+
+    });
 
     this.el.myPhoto.on('click', e => {
 
